f3e2ee745e37651bbeb2e97ae69c1f54dc0048d3
Alexxxx1013 <alessandrobaccani03@gmail.com>
2025-02-24 16:34:38 +0100

    Movement fix

--- Assets/Code/Score/Scoring.cs
+++ Assets/Code/Score/Scoring.cs
@@ -3,17 +3,47 @@ using UnityEngine;
 
 public class Scoring : MonoBehaviour
 {
-    public TMP_Text scoreText; // UI per il punteggio
+    public TMP_Text scoreText;
+    public Transform player;
 
-    int score = 0;
+    private int score = 0;
+    private float minDistance = 0f;
+
+    void Start()
+    {
+        if (player != null)
+        {
+            minDistance = player.position.x;
+        }
+        UpdateScoreText();
+    }
+
+    void Update()
+    {
+        UpdateScore();
+    }
 
     public void UpdateScore()
     {
-        score++;
+        if (player != null)
+        {
+            float currentDistance = player.position.x;
 
+            if (currentDistance < minDistance)
+            {
+                minDistance = currentDistance;
+                score++;
+                UpdateScoreText();
+            }
+        }
+    }
+
+    void UpdateScoreText()
+    {
         if (scoreText != null)
         {
             scoreText.text = "Score: " + score;
         }
     }
 }
+
