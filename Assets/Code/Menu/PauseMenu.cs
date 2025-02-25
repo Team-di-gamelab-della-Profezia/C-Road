f28db50444ca203eb27da8023ce05d2879b2a731
Deva <manuelbocelli1@gmail.com>
2025-02-26 00:19:07 +0100

    UI

---
+++ Assets/Code/Menu/PauseMenu.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [SerializeField]GameObject pause_menu;
+    public void Pause()
+    {
+        pause_menu.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void home()
+    {
+        SceneManager.LoadScene("Main Menu");
+        Time.timeScale = 1;
+    }
+    public void resume()
+    {
+        pause_menu.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+}
