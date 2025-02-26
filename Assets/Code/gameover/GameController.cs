3271edc4fb50b3392632c1a587645e58337287e8
Deva <manuelbocelli1@gmail.com>
2025-02-26 19:18:13 +0100

    Game Over
    quasi finito
    
    (sto impazzendo non funziona niente)

---
+++ Assets/Code/gameover/GameController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameOverScreen gameOverScreen;
+    int maxPlatform = 0;
+
+    public void GameOver()
+    {
+        
+    }
+
+    
+}
