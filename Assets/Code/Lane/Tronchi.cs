01c28be00d892e6d6d93b1c06ff419671883dc22
Alexxxx1013 <alessandrobaccani03@gmail.com>
2025-02-27 15:20:42 +0100

    Fix collisions

---
+++ Assets/Code/Lane/Tronchi.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Tronchi : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            other.transform.SetParent(transform);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            other.transform.SetParent(null);
+    }
+}
